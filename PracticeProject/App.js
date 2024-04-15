7c7de556a926a8b9838c20a6499edd5ec807152f
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-04-16 00:05:11 +0500

    2

--- PracticeProject/App.js
+++ PracticeProject/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableWithoutFeedback, Button, StyleSheet } from 'react-native';
+import {usestate} from 'react';
 
 const App = () => {
+  const handlePress = () => {
+    console.log('Button Pressed');
+  }
+
+
   return(
+    //View 
     <View styles={styles.container} >
-      <Text>Hello World</Text>
+
+      //Text
+      <Text onPress={handlePress}>Hello World</Text>
+
+      //TouchableWithoutFeedback
+      <TouchableWithoutFeedback onPress={() => {console.log("image tapped")} }>
+
+      //images
+      <image source={require("./.bundle")}></image>
+      <image source={require("./.bundle")}/>
+      //Network images
+      <image source={{uri: "http...."}}></image>
+
+      </TouchableWithoutFeedback>
+
+      //Button
+      <button
+      title="click me"
+      />
+
     </View>
   )
 
@@ -12,7 +38,7 @@ const App = () => {
 
 
 const styles=StyleSheet.create({
-  
+
   container:{
     flex:1,
     justifyContent: "center",
